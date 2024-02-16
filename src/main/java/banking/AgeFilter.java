142603b8962cd1f4f75adab9b729116d59584db0
Viateur <akimanaviateur94@gmail.com>
2024-02-16 20:44:34 +0200

    Almost finished banking system

--- src/main/java/banking/AgeFilter.java
+++ src/main/java/banking/AgeFilter.java
@@ -1,4 +1,33 @@
 package banking;
 
-public class AgeFilter {
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import java.io.IOException;
+
+public abstract class AgeFilter implements Filter {
+    public void doFilter(jakarta.servlet.ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String ageStr = request.getParameter("age");
+        if(ageStr != null && !ageStr.isEmpty()) {
+            try {
+                int age = Integer.parseInt(ageStr);
+                if(age > 18) {
+                    chain.doFilter(request,response);
+                } else {
+                    request.setAttribute("errorMessage","You must be at least 18 years old to register");
+                    request.getRequestDispatcher("Register.jsp").forward(request, response);
+                }
+            } catch (NumberFormatException e) {
+                request.setAttribute("errorMessage", "Invalid age format.");
+                request.getRequestDispatcher("Register.jsp").forward(request, response);
+            }
+
+        } else {
+            request.setAttribute("errorMessage", "Age is required.");
+            request.getRequestDispatcher("Register.jsp").forward(request, response);
+        }
+    }
 }
